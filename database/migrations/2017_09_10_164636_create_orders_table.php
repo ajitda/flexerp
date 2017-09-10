ecf54010ec8d2b6d66155acb8b229e2952e8b5a7
Admin <ajitdas2900@gmail.com>
2017-09-10 03:57:47 -0700

    all migration file added

--- database/migrations/2017_09_10_164636_create_orders_table.php
+++ database/migrations/2017_09_10_164636_create_orders_table.php
@@ -16,6 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('qty');
+            $table->string('description');
             $table->integer('unit_price');
             $table->timestamps();
         });
