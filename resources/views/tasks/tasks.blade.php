463e61b329ab2f884f54fe80d6ee7f224e6f5d6b
ajitda <ajitdas2900@gmail.com>
2017-11-04 22:44:22 +0600

    task view file added

--- resources/views/tasks/tasks.blade.php
+++ resources/views/tasks/tasks.blade.php
@@ -1,1 +1,19 @@
 @extends('layouts.app')
+@section('content')
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h1>Task List</h1>
+		</div>
+		<div class="panel-body">
+			<table class="table table-bordered table-striped table-hover">
+				<thead>
+					<th>Sl.</th>
+					<th>Task Name</th>
+					<th>Description</th>
+					<th>Assigned To</th>
+					<th>Status</th>
+				</thead>
+			</table>
+		</div>
+	</div>
+@endsection
