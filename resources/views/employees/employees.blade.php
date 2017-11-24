58a402ce6ee5a4f965e8464b2fc13590e858b6f4
ajitda <ajitdas2900@gmail.com>
2017-11-24 23:16:37 +0600

    list file modified

--- resources/views/employees/employees.blade.php
+++ resources/views/employees/employees.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h1>Employees List<a href="employees/create" class="pull-right create-button"><span class="glyphicon-plus"></span></a></h1>
+                <h1>Employees List<a href="employees/create" class="pull-right create-button"><span class="glyphicon glyphicon-plus"></span></a></h1>
             </div>
             <div class="panel-body">
                 <table class="table table-bordered table-striped table-hover">
