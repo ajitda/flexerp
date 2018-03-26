d524922cee397a9518ccb41adf54101b755ef839
ajit57 <ajitdas2900@gmail.com>
2018-03-26 23:22:56 +0600

    invoice database modified

--- database/migrations/2018_03_20_225455_create_invoices_table.php
+++ database/migrations/2018_03_20_225455_create_invoices_table.php
@@ -15,9 +15,17 @@ class CreateInvoicesTable extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->increments('id');
+
             $table->decimal('amount',12,2);
             $table->decimal('payment',12,2);
             $table->decimal('dues',12,2);
+            $table->date('payment_date');
+            $table->date('due_date');
+
+            $table->integer('customer_id');
+
+            $table->integer('service_id');
+            
             $table->timestamps();
         });
     }
