13344edd4d2d25337f3349cc9f00a1ed938748e8
ajit57 <ajitdas2900@gmail.com>
2018-04-19 23:45:59 +0600

    tinymce added

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -21,7 +21,7 @@
 					{!! Form::select('status', ['pending'=> 'Pending', 'completed'=> 'Completed', 'later'=>'Do Later'], null, ['required', 'class'=>'form-control']) !!}
 				</div>
 				<div class="form-group col-sm-12">
-					{!! Form::textarea('description', null, ['required', 'class'=>'form-control address', 'placeholder'=> 'Enter Task Description']) !!}
+					{!! Form::textarea('description', null, ['required', 'class'=>'form-control address', 'id'=>'mytextarea', 'placeholder'=> 'Enter Task Description']) !!}
 				</div>
 				<div class="form-group col-sm-3">
 					{!! Form::label('employee_id', 'Assigned To', ['class'=>'label']) !!}
@@ -33,4 +33,11 @@
 			{!! Form::close() !!}
 		</div>
 	</div>
+@endsection
+@section('scripts')
+	<script>
+        var editor_config = { path_absolute : "{{ URL::to('/') }}/",
+            selector : '#mytextarea',
+            plugins: [ "advlist autolink lists link image charmap print preview hr anchor pagebreak", "searchreplace wordcount visualblocks visualchars code fullscreen", "insertdatetime media nonbreaking save table contextmenu directionality", "emoticons template paste textcolor colorpicker textpattern" ], toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media", relative_urls: false, file_browser_callback : function(field_name, url, type, win) { var x = window.innerWidth || document.documentElement.clientWidth || document.getElementByTagName('body')[0].clientWidth; var y = window.innerHeight|| document.documentElement.clientHeight|| document.grtElementByTagName('body')[0].clientHeight; var cmsURL = editor_config.path_absolute+'laravel-filemanaget?field_name'+field_name; if (type = 'image') { cmsURL = cmsURL+'&type=Images'; } else { cmsUrl = cmsURL+'&type=Files'; } tinyMCE.activeEditor.windowManager.open({ file : cmsURL, title : 'Filemanager', width : x * 0.8, height : y * 0.8, resizeble : 'yes', close_previous : 'no' }); } }; tinymce.init(editor_config);
+	</script>
 @endsection
