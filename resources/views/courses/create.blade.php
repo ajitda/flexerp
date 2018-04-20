13a62fcf48b221780b187ebd276eb0c3b59b93f6
ajit57 <ajitdas2900@gmail.com>
2018-04-20 23:21:41 +0600

    tinymce added in the create course

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -31,7 +31,7 @@
                         </div>
 
                         <div class="form-group col-md-12">
-                            {!! Form::textarea('description', null, array('required', 'class'=>'form-control address', 'placeholder'=>'Course Description')) !!}
+                            {!! Form::textarea('description', null, array('required', 'id'=>'mytextarea', 'class'=>'form-control address', 'placeholder'=>'Course Description')) !!}
                         </div>
 
                         <div class="form-group col-md-3">
@@ -43,5 +43,11 @@
         </div>
 
     </div>
-
-    @endsection
+@endsection
+@section('scripts')
+    <script>
+        var editor_config = { path_absolute : "{{ URL::to('/') }}/",
+            selector : '#mytextarea',
+            plugins: [ "advlist autolink lists link image charmap print preview hr anchor pagebreak", "searchreplace wordcount visualblocks visualchars code fullscreen", "insertdatetime media nonbreaking save table contextmenu directionality", "emoticons template paste textcolor colorpicker textpattern" ], toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media", relative_urls: false, file_browser_callback : function(field_name, url, type, win) { var x = window.innerWidth || document.documentElement.clientWidth || document.getElementByTagName('body')[0].clientWidth; var y = window.innerHeight|| document.documentElement.clientHeight|| document.grtElementByTagName('body')[0].clientHeight; var cmsURL = editor_config.path_absolute+'laravel-filemanaget?field_name'+field_name; if (type = 'image') { cmsURL = cmsURL+'&type=Images'; } else { cmsUrl = cmsURL+'&type=Files'; } tinyMCE.activeEditor.windowManager.open({ file : cmsURL, title : 'Filemanager', width : x * 0.8, height : y * 0.8, resizeble : 'yes', close_previous : 'no' }); } }; tinymce.init(editor_config);
+    </script>
+@endsection
